5acb35e1d9660ad98eba2bb4ca5c4c44c7686500
b-masood <bmasood@gmail.com>
2022-02-02 17:04:56 +1300

    Add comments

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    /* Validate principal and display alert if needed */
     if (principal <= 0)
     {
         alert("Please enter a positive number");
         principal.focus();
         return false;
     }
-    else 
+    else
+    /* No need to execute this for failed validation */ 
     {
         var rate = document.getElementById("rate").value;
         var years = document.getElementById("years").value;
         var interest = principal * years * rate /100;
         var year = new Date().getFullYear()+parseInt(years);
+        /* Set innder text. Use mark element for highlighting numbers. */ 
         document.getElementById("result").innerHTML="If you deposit \<mark>"+principal+"\</mark>,\<br\>at an interest rate of \<mark>"+rate+"%\</mark>\<br\>You will receive an amount of \<mark>"+interest+",\</mark>\<br\>in the year \<mark>"+year+"\</mark>\<br\>";
         return true;
     }
